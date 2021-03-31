2219481f70492b6ffa0d345e964caf8c8d924ab9
yjh <yjh465402634@gmail.com>
2021-03-31 18:03:43 +0800

    chore: arrange code (#4)

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,7 @@
 export * from "./option";
+export * from "./optionable";
 export * from "./result";
+export * from "./resultable";
 export * from "./box";
 export * from "./shared";
 import * as util from "./util";
