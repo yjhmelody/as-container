c5f5ea1a8425a3c4127a28e26e845fe1bcd19f69
yjh <yjh465402634@gmail.com>
2023-02-16 20:26:10 +0800

    ci: improve dependentabot and release (#49)
    * improve ci
    
    * improve
    
    * fix release.txt
    
    * improve cron for auto merge
    
    * chore: improve ci schedule and make Result constructor protected

--- assembly/util.ts
+++ assembly/util.ts
@@ -1,15 +1,21 @@
-// @ts-ignore
+// @ts-nocheck
+
+/**
+ * Instance a zero value of type T.
+ *
+ * Return zero value if it is primitive type.
+ * Return (unsafe) null if it is reference type.
+ * Otherwise try to call constructor without arguments for it.
+ * @returns zero value of T
+ */
 @inline
 @unsafe
 export function instantiateZero<T>(): T {
     if (isFloat<T>()) {
-        // @ts-ignore
         return 0;
     } else if (isInteger<T>()) {
-        // @ts-ignore
         return 0;
     } else if (isBoolean<T>()) {
-        // @ts-ignore
         return false;
     } else if (isReference<T>()) {
         return changetype<T>(0);
